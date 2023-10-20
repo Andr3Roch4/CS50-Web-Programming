b1d94943fdd00baff54e613147ebfc8f572ab648
André Rocha <andre.rcrocha@gmail.com>
2023-10-20 12:57:56 +0100

    added createnew, edit, random and markdown convert

--- project1/encyclopedia/urls.py
+++ project1/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:title>", views.title, name="title"),
-    path("search/", views.searchresult, name="search")
+    path("search/", views.searchresult, name="search"),
+    path("random/", views.randomtitle, name="random")
     ]
--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+import random
+from markdown2 import Markdown
 
 
 from . import util
@@ -24,8 +26,10 @@ def index(request):
 
 def title(request, title):
         if util.get_entry(title):
+            markdowner = Markdown()
+            titletext = markdowner.convert(util.get_entry(title))
             return render(request, "encyclopedia/title.html", {
-                "titletext": util.get_entry(title),
+                "titletext": titletext,
                 "title": title
             })
         elif not util.get_entry(title):
@@ -43,3 +47,31 @@ def searchresult(request):
         return render(request, "encyclopedia/searchresult.html", {
             "results": results
         })
+    
+def createnew(request):
+    content = request.GET.get("content")
+    title = request.GET.get("title")
+    if str.lower(title) in str.lower(util.list_entries()):
+        return render(request, "encyclopedia/titleexists.html", {
+             "content": content
+        })
+    else:
+        util.save_entry(title, content)
+        return HttpResponseRedirect(f"/{title}")
+    
+def editpage(request):
+    title = request.GET.get("title")
+    if request.method == "GET":
+        content = util.get_entry(title)
+        return render(request, "encyclopedia/editpage.html", {
+            "content": content,
+            "title": title
+        })
+    elif request.method == "POST":
+        newcontent = request.POST.get("newcontent")
+        util.save_entry(title, newcontent)
+        return HttpResponseRedirect(f"/{title}")
+
+def randomtitle(request):
+    title = random.choice(util.list_entries())
+    return HttpResponseRedirect(f"/{title}")
